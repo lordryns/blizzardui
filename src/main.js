51bf2b7b690e8f0ec373e468b24c15d6cc3a5f37
David <rynstheoverlord@gmail.com>
2025-07-30 01:15:48 +0100

    initial commit

---
+++ src/main.js
@@ -0,0 +1,3 @@
+import 'fomantic-ui-css/semantic.min.css';
+import 'fomantic-ui-css/semantic.min.js';
+
---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Blizzard UI',
+        short_name: 'Blizzard',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#2185d0', // Semantic/Fomantic primary blue
+        icons: [
+          {
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+});
