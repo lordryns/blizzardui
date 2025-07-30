1a6df94a99e5c1bf6c1e60334b52f70493fabbbf
David <rynstheoverlord@gmail.com>
2025-07-30 01:32:42 +0100

    blueprint

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
 import 'fomantic-ui-css/semantic.min.css';
 import 'fomantic-ui-css/semantic.min.js';
 
+
+const createStoreBtn = document.getElementById("create-btn");
+
+
+createStoreBtn.onclick = function () {
+  $.toast({
+    class: 'success',
+    message: `Store created successfully!`
+  })
+;
+}
+
+
