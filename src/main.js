f7494299cab47b6d976fe85c83c9aad56eb94b91
David <rynstheoverlord@gmail.com>
2025-07-30 19:55:17 +0100

    update

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
+import "/src/style.css";
 import 'fomantic-ui-css/semantic.min.css';
 import 'fomantic-ui-css/semantic.min.js';
-
+import axios from "axios";
 
 const createStoreBtn = document.getElementById("create-btn");
+const createEditGroup = document.getElementById("create-edit-group");
 
 
-createStoreBtn.onclick = function () {
-  $.toast({
-    class: 'success',
-    message: `Store created successfully!`
-  })
-;
+var base_url = "https://weblizzard.onrender.com";
+createStoreBtn.onclick = async function () {
+  createEditGroup.innerHTML = "Loading...";
+  try {
+    var res = await axios.post(base_url + "/create_store")
+    createEditGroup.innerHTML = res.data.id;
+    $.toast({
+      class: 'success',
+      message: `Store created successfully!`
+    })
+
+  } catch (err) {
+    console.log(err)
+  }
+ ;
 }
 
 
