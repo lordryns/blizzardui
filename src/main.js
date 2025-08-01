a38a3e7942be0c0d837c70fc67eb0dfc3a6db6d7
David <rynstheoverlord@gmail.com>
2025-08-02 00:23:40 +0100

    Added new ui

--- src/main.js
+++ src/main.js
@@ -3,25 +3,42 @@ import 'fomantic-ui-css/semantic.min.css';
 import 'fomantic-ui-css/semantic.min.js';
 import axios from "axios";
 
-const createStoreBtn = document.getElementById("create-btn");
-const createEditGroup = document.getElementById("create-edit-group");
+let storeId = null;
 
+const createStoreBtn = document.getElementById("create-new-btn");
+const loadStoreBtn = document.getElementById("load-store-btn");
 
-var base_url = "https://weblizzard.onrender.com";
+const webContent = document.getElementById("content");
+
+let base_url = "https://weblizzard.onrender.com";
+
 createStoreBtn.onclick = async function () {
-  createEditGroup.innerHTML = "Loading...";
+  webContent.innerHTML = formanticLoader();
   try {
-    var res = await axios.post(base_url + "/create_store")
-    createEditGroup.innerHTML = res.data.id;
+    let res = await axios.post(base_url + "/create_store")
+    webContent.innerHTML = res.data.id;
     $.toast({
       class: 'success',
-      message: `Store created successfully!`
+      title: 'Success',
+      message: 'Store created successfully!',
+      showProgress: 'bottom'
     })
-
-  } catch (err) {
-    console.log(err)
+    ;
+  } catch(err) {
+    webContent.innerHTML = String(err);
   }
- ;
 }
 
+loadStoreBtn.onclick = function () {
+  $('#load-store-modal')
+    .modal('show')
+  ;
+}
 
+function formanticLoader() {
+  return `<div class="ui active dimmer">
+    <div class="ui medium text loader">Loading</div>
+  </div>
+  <p></p>
+  <p></p>`
+}
